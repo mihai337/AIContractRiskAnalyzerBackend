c6afc9869b581d04c5c21912caa23aa28a1ff5ba
Mihai Constantin <constantinm7787@gmail.com>
2026-03-26 17:30:57 +0200

    init

---
+++ src/main/java/licenta/mihai/aicontractriskanalyzerbackend/AiContractRiskAnalyzerBackendApplication.java
@@ -0,0 +1,13 @@
+package licenta.mihai.aicontractriskanalyzerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiContractRiskAnalyzerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiContractRiskAnalyzerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/licenta/mihai/aicontractriskanalyzerbackend/AiContractRiskAnalyzerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package licenta.mihai.aicontractriskanalyzerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiContractRiskAnalyzerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
